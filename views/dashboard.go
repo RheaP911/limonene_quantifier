1c62454fcf8fc3ad17e131c249ba1b8e1f18a185
RheaP911 <rhealynpanganiban20@gmail.com>
2024-04-22 12:32:25 +0800

    Fetched data from Images model

--- views/dashboard.go
+++ views/dashboard.go
@@ -3,10 +3,17 @@ package views
 import (
 	"net/http"
 	"strings"
+
+	"github.com/RheaP911/limonene_quantifier/models"
+	"github.com/uadmin/uadmin"
 )
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	c := map[string]interface{}{}
+	images := []models.Images{}
+
+	uadmin.All(&images)
+	c["Images"] = images
 
 	c["Title"] = "Dashboard | Page"
 
--- views/images.go
+++ views/images.go
@@ -3,10 +3,20 @@ package views
 import (
 	"net/http"
 	"strings"
+
+	"github.com/RheaP911/limonene_quantifier/models"
+	"github.com/uadmin/uadmin"
 )
 
 func ImagesHandler(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	c := map[string]interface{}{}
+	images := []models.Images{}
+
+	uadmin.All(&images)
+	for x := range images {
+		uadmin.Preload(&images[x])
+	}
+	c["Images"] = images
 
 	c["Title"] = "Images | Page"
 
